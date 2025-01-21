fecc123f964e31158b0d2cdcb51baecc34f4d052
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-01-21 22:51:14 +0100

    Added the TodoItem.cs file containing the Model of a todo item

--- TodoApi/Models/TodoItem.cs
+++ TodoApi/Models/TodoItem.cs
@@ -1,9 +1,29 @@
+using System
+using System.ComponentModels.DataAnnotations
+
+
 namespace TodoApi.Models
 {
   public class TodoItem
   {
     public long Id { get; set; }
-    public string? Name { get; set; }
-    public bool IsCompleted { get; set; }
+
+    [Required(ErrorMessage = "Name is Required")]
+    [StringLength(200, ErrorMessage = "Name cannot exceed 200 characters")]
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public PriorityLevel Priority { get; set; }
+    public bool IsCompleted { get; set; } = false;
+    public DateTime DueDate { get; set; } = DateTime.MaxValue;
+    public DateTime CompletedAt { get; set; }
+    public DateTime CreatedAt { get; }
+    public DateTime UpdatedAt { get; }
+  }
+
+  public enum PriorityLevel
+  {
+    Low,
+    Medium,
+    High
   }
 }
