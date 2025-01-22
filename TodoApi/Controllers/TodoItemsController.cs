3043207e333b474274f132675e517b4ba57a83b5
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-01-22 22:56:51 +0100

    Added first few lines of TodoItems controller

---
+++ TodoApi/Controllers/TodoItemsController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc
+using Microsoft.EntityFrameworkCore
+using TodoApi.Models
+
+namespace TodoApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class TodoItemsController : ControllerBase
+  {
+    private readonly TodoContext _context;
+    public TodoItemsController(TodoContext context)
+    {
+      _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+    {
+      return await _context.TodoItems.ToStringAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+    {
+      var todoItem
+
