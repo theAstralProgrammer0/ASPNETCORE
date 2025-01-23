c85efd25e4c153056629d54322df39a926c11714
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-01-23 02:52:43 +0100

    Checkpoint: ASP.NET Core Dockerized WebAPI Build Successful

--- TodoApi/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67302dd1ac740c8069d50579b2bdc30b7f37ad49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7709c92daab9fcad6faef9437ac427650b49ec57")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
